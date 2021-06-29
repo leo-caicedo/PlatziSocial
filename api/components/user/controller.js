4bdf2801c0617b9f381d8fc0793a410fa5d4e076
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-29 15:35:30 -0500

    Autenticación: registro

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,37 +1,48 @@
-const nanoid = require('nanoid');
+const { nanoid } = require('nanoid')
+const auth = require("../auth/index");
 const TABLA = 'user';
 
 module.exports = function (injectedStore) {
-    let store = injectedStore;
+  let store = injectedStore;
 
-    if (!store) {
-        store = require('../../../store/dummy');
-    }
+  if (!store) {
+    store = require("../../../store/dummy");
+  }
 
-    function list() {
-        return store.list(TABLA);
-    }
+  function list() {
+    return store.list(TABLA);
+  }
 
-    function get(id) {
-        return store.get(TABLA, id);
-    }
+  function get(id) {
+    return store.get(TABLA, id);
+  }
 
-    function upsert(body) {
-        const user = {
-            name: body.name
-        }
-
-        if (body.id) {
-            user.id = body.id;
-        } else {
-            user.id = nanoid();
-        }
-
-        return store.upsert(TABLA, user);
-    }
-
-    return {
-        list,
-        get,
+  async function upsert(body) {
+    const user = {
+      name: body.name,
+      username: body.username,
     };
-}
+
+    if (body.id) {
+      user.id = body.id;
+    } else {
+      user.id = nanoid();
+    }
+
+    if (body.password || body.username) {
+      await auth.upsert({
+	id: user.id,
+	username: user.username,
+	password: body.password,
+      })
+    }
+
+    return store.upsert(TABLA, user);
+  }
+
+  return {
+    list,
+    get,
+    upsert,
+  };
+};
--- store/dummy.js
+++ store/dummy.js
@@ -1,26 +1,31 @@
 const db = {
-    user: [
-        { id: '1', name: "Leonardo" },
-        { id: '2', name: "Said" },
-        { id: '3', name: "Juana" },
-    ],
+  user: [
+    { id: "1", name: "Leonardo" },
+    { id: "2", name: "Said" },
+    { id: "3", name: "Juana" },
+  ],
 };
 
 async function list(tabla) {
-    return db[tabla];
+  return db[tabla];
 }
 
 async function get(tabla, id) {
-    let collection = await list(tabla);
-    return collection.filter(item => item.id === id)[0] || null;
+  let collection = await list(tabla);
+  return collection.filter((item) => item.id === id)[0] || null;
 }
 
 async function upsert(tabla, data) {
-    //
+  if (!db[tabla]) {
+    db[tabla] = [];
+  }
+
+  db[tabla].push(data);
+  console.log(db);
 }
 
 async function remove(tabla, id) {
-    //
+  //
 }
 
 module.exports = {
