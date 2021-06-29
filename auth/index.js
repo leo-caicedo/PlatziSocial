d2f70091a43b15a8d3e29c99292620f0d44d607e
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-29 18:13:14 -0500

    Autenticación: gestión de permisos

--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,43 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const secret = config.jwt.secret;
 
 function sign(data) {
-  return jwt.sign(data, 'SECRET')
+  return jwt.sign(data, secret);
+}
+
+function sign(data) {
+  return jwt.sign(data, secret);
+}
+
+const check = {
+  own: function (req, owner) {
+    const decoded = decodeHeader(req);
+    console.log(decoded);
+  },
+};
+
+function getToken(auth) {
+  if (!auth) {
+    throw new Error("No viene Tokem");
+  }
+
+  if (auth.indexOf("Bearer ") === -1) {
+    throw new Error("Formato invalido");
+  }
+
+  let token = auth.replace("Bearer ", "");
+  return token;
+}
+
+function decodeHeader(req) {
+  const authorization = req.headers.authorization || "";
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded();
+
+  return decoded;
 }
 
 module.exports = {
