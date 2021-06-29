db706dcacc81d1392ba40574d8dc09ae3761499e
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-29 09:52:22 -0500

    Estructura inicial del proyecto: API y rutas

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const config = require('../config');
+const user = require('./components/user/network');
+
+// ROUER
+app.use('/api/user', user);
+
+app.listen(config.api.port, () => {
+    console.log(`API escuchando en el puerto ${config.api.port}`);
+});
