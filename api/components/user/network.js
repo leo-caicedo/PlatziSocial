0d5f5c0dac328e2538dbf910eaf24cb80723002a
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-30 11:26:30 -0500

    Base de datos real: MySQL

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,49 +1,40 @@
-const express = require('express');
+const express = require("express");
 
-const secure = require('./secure');
-const response = require('../../../network/response');
-const Controller = require('./index');
+const secure = require("./secure");
+const response = require("../../../network/response");
+const Controller = require("./index");
 
 const router = express.Router();
 
 // Routes
-router.get('/', list)
-router.get('/:id', get);
-router.post('/', upsert);
-router.put('/', secure("update"), upsert);
+router.get("/", list);
+router.get("/:id", get);
+router.post("/", upsert);
+router.put("/", secure("update"), upsert);
 
 // Internal functions
-function list(req, res) {
-    Controller.list()
-        .then((lista) => {
-            response.success(req, res, lista, 200);
-        })
-        .catch((err) => {
-            response.error(req, res, err.message, 500);
-        });
-    
+function list(req, res, next) {
+  Controller.list()
+    .then((lista) => {
+      response.success(req, res, lista, 200);
+    })
+    .catch(next);
 }
 
-function get(req, res) {
-    Controller.get(req.params.id)
-        .then((user) => {
-            response.success(req, res, user, 200);
-        })
-        .catch((err) => {
-            response.error(req, res, err.message, 500);
-        });
-    
+function get(req, res, next) {
+  Controller.get(req.params.id)
+    .then((user) => {
+      response.success(req, res, user, 200);
+    })
+    .catch(next);
 }
 
-function upsert(req, res) {
-    Controller.upsert(req.body)
-        .then((user) => {
-            response.success(req, res, user, 201);
-        })
-        .catch((err) => {
-            response.error(req, res, err.message, 500);
-        });
-    
+function upsert(req, res, next) {
+  Controller.upsert(req.body)
+    .then((user) => {
+      response.success(req, res, user, 201);
+    })
+    .catch(next);
 }
 
 module.exports = router;
--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
   jwt: {
     secret: process.env.JWT_SECRET || "SECRET",
   },
+  mysql: {
+    host: process.env.MYSQL_HOST || "remotemysql.com",
+    user: process.env.MYSQL_USER || "1HPo3y0Ryu",
+    password: process.env.MYSQL_PASS || "xU7qPKAhx6",
+    database: process.env.MYSQL_DB || "1HPo3y0Ryu",
+  },
 };
